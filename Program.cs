46f9f05d62a0deeec9721444f0395aba40a55a9e
mortdaabbas <mortdabbs@gmail.com>
2024-09-27 15:23:31 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linked_list
+{
+    internal class Program
+    {
+        class linkedlistnode
+        {
+            public int data;
+            public linkedlistnode next;
+            public linkedlistnode(int x) 
+            {
+                data = x;
+                next = null;
+            }
+        }
+
+        class linkedlist
+        {
+           static int count;
+
+           static linkedlistnode head;
+            static linkedlist()
+            {
+                head = null;
+                count = 0;
+            }
+            public void AddNodetofront(int data)
+            {
+                linkedlistnode node = new linkedlistnode(data);
+                node.next = head;
+                head = node;
+                count++;
+            }
+            public void printlist()
+            {
+                linkedlistnode runner = head;
+                while (runner != null)
+                {
+                    Console.WriteLine(runner.data);
+                    runner = runner.next;
+                }
+            }
+            public int Find(int x) 
+            {
+                linkedlistnode f = head;
+                while (f != null)
+                {
+                    if (f.data == x)
+                        return 1;
+                    f = f.next;
+                }
+                return -1;
+            }
+           
+        }
+
+        static void Main(string[] args)
+        {
+      
+            linkedlist linkedlist = new linkedlist();
+            for (int i = 0; i < 5; i++)
+            {
+                linkedlist.AddNodetofront(i);
+            }
+            linkedlist.printlist();
+            linkedlist n1 = new linkedlist();
+           int f= n1.Find(2);
+            if (f != -1)
+                Console.WriteLine("found");
+            else
+                Console.WriteLine("not found");
+        }
+    }
+}
